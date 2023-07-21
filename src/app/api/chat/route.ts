f1597d6adbd26cf9ec016d40af311ad693bac459
Martin Funcich <martin.funcich@ie.com.au>
2023-07-21 11:30:06 +1000

    feat: Support Azure OpenAI

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -5,13 +5,19 @@ export async function POST(request: Request) {
   const postBody = await request.json() as { messages: OpenAiMessages };
   const messages = postBody.messages;
 
-  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+  const isAzure = `${process.env.OPENAI_API_ENDPOINT}`.includes("azure.com");
+  const headers: Record<string, string> = !isAzure ? {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+    "OpenAI-Organization": "org-IHas545MzA37I7rtXXWZBQS1"
+  } : {
+    "Content-Type": "application/json",
+    "api-key": `${process.env.OPENAI_API_KEY}`,
+  };
+
+  const res = await fetch(`${process.env.OPENAI_API_ENDPOINT}`, {
     "method": "POST",
-    "headers": {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
-      "OpenAI-Organization": "org-IHas545MzA37I7rtXXWZBQS1"
-    },
+    headers,
     body: JSON.stringify({
       "model": "gpt-3.5-turbo",
       "messages": messages,
